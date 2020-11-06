15563437d563230ab7f430a3b3d74a70afca03c0
Andrea Pitto <andreabradpitto@gmail.com>
2020-11-07 00:01:05 +0100

    Final (2) adjustments
    Some little improvements as the assignment's deadline has been postponed

--- assignment1/src/control.py
+++ assignment1/src/control.py
@@ -18,9 +18,7 @@ sim_scale = rospy.get_param('sim_scale')
 def control_cb(data):
     #time.sleep(random.randint(2, 3) / sim_scale)
     pub = rospy.Publisher('motion_over_topic', Coordinates, queue_size=10)
-    time.sleep(random.randint(2, 3) / sim_scale) # I do not understand why it works
-                                                 # only if I put a wait here,
-                                                 # but it does
+    time.sleep(random.randint(2, 3) / sim_scale)
     pub.publish(data)
 
 ## Initializes the control_node and subscribes to the control_topic. manager_listener
